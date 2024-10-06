01abad2b5279d2024bb80ec07a6d427978071c78
Chris D.C <94999762+ChristianDziurza@users.noreply.github.com>
2024-10-06 17:53:18 -0300

    Add files via upload
    Tudo feito :)

--- Aula19/Exercício 10/exercicio10.cpp
+++ Aula19/Exercício 10/exercicio10.cpp
@@ -7,15 +7,21 @@ int main(){
     int a, b;
     cin >> a >> b;
     cout << a << " --- " << b << endl;
-    int *p1=&a, *p2=&b;
-
+    int *p1, *p2;
+    if(a%2 != 1){
+        p1 = &a;
+        p2 = &b;
+    }else {
+        p1 = &b;
+        p2 = &a;
+    }
     if(Comparacao(p1,p2))
-        cout << *p1 << " eh maior que " << *p2 << endl;
+        cout << "A variavel A esta em um endereco maior" << endl;
     else
-        cout << *p2 << " eh maior que " << *p1 << endl;
+        cout << "A variavel B esta em um endereco maior" << endl;
     return 0;
 }
 
 bool Comparacao(int *v1, int *v2){
-    return (*v1 > *v2)? true : false;
+    return (v1 > v2)? true : false;
 }
